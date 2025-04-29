e7a6797f3b23611d5d2857d17bd8c776cea824fd
fckndrg51 <fister909@mail.ru>
2025-04-29 11:34:00 +0500

    feat: добавил бизнес и орм модели

--- src/storage/sqlalchemy/tables/jobs.py
+++ src/storage/sqlalchemy/tables/jobs.py
@@ -1,5 +1,7 @@
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
+
 
 from storage.sqlalchemy.client import Base
 
@@ -13,6 +15,12 @@ class Job(Base):
     )
 
     # добавьте ваши колонки сюда
+    title: Mapped[str] = mapped_column(comment="Название вакансии")
+    description: Mapped[str] = mapped_column(comment="Описание вакансии")
+    salary_from: Mapped[str] = mapped_column(comment="Зарплата от")
+    salary_to: Mapped[str] = mapped_column(comment="Зарплата до")
+    is_active: Mapped[bool] = mapped_column(default=True, comment="Активна ли вакансия")
+    created_at: Mapped[datetime] = mapped_column(comment="Время создания записи", default=datetime.utcnow)
 
     user: Mapped["User"] = relationship(back_populates="jobs")  # noqa
     responses: Mapped["Response"] = relationship(back_populates="job")  # noqa
